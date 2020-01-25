17c1c8ae51da21d0c7ac4511468d69abd7b68ab9
Haotian <htsli@ucdavis.edu>
2020-01-24 22:29:46 -0800

    find the proper xlims, ylims, zlims

--- develop/visualize.jl
+++ develop/visualize.jl
@@ -1,8 +1,15 @@
-using LightGraphs, Plots, JLD
+using LightGraphs, Plots, JLD, Statistics
 
 include("..\\src\\gplot.jl")
+include("..\\src\\Centerized.jl")
 
 G = loadgraph(".\\data\\cat8_simplified_vert503.lgz")
-X = load(".\\data\\cat8_simplified_vert503_xyz.jld", "xyz")
+X = Centerized(load(".\\data\\cat8_simplified_vert503_xyz.jld", "xyz"))
 
 scatter(X[:,1], X[:,2], X[:,3], aspect_ratio = 1)
+
+gplot(1.0*adjacency_matrix(G), X); plot!(aspect_ratio = 1, xlims = [-100, 100], ylims = [-100, 100], zlims = [-100, 100])
+
+for i = 1:3
+    print(maximum(X[:,i]), ", ", minimum(X[:,i]), "; ")
+end
